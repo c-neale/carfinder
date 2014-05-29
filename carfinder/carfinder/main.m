a874600dfa96454abad355d5d7a4d54d8f22d742
Cory Neale <cory.neale@gmail.com>
2014-05-29 12:33:59 +1000

    Added empty project

---
+++ carfinder/carfinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  carfinder
+//
+//  Created by Cory Neale on 29/05/2014.
+//  Copyright (c) 2014 Cory Neale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ carfinder/carfinderTests/carfinderTests.m
@@ -0,0 +1,34 @@
+//
+//  carfinderTests.m
+//  carfinderTests
+//
+//  Created by Cory Neale on 29/05/2014.
+//  Copyright (c) 2014 Cory Neale. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface carfinderTests : XCTestCase
+
+@end
+
+@implementation carfinderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
