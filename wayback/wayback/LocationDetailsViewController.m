d3476a3cf25c42fe3de43a3684f8f57ec9f0e3e7
Cory Neale <cory.neale@gmail.com>
2014-06-15 22:16:30 +1000

    fixed obscured keyboard
    added code to listen for keyboard notifications and move the view when
    keyboard appears.

--- wayback/wayback/LocationDetailsViewController.m
+++ wayback/wayback/LocationDetailsViewController.m
@@ -16,6 +16,11 @@
     UIBarButtonItem * commitEditButton;
 }
 
+- (void) registerForKeyboardNotifications;
+- (void) deregisterForKeyboardNotifications;
+- (void) keyboardWillShow:(NSNotification *) notify;
+- (void) keyboardWillBeHidden:(NSNotification *) notify;
+
 @end
 
 @implementation LocationDetailsViewController
@@ -63,11 +68,15 @@
     [super viewWillAppear:animated];
     
     self.screenName = @"LocationDetailsViewController";
+    
+    [self registerForKeyboardNotifications];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
     [super viewDidDisappear:animated];
+    
+    [self deregisterForKeyboardNotifications];
 }
 
 - (void)didReceiveMemoryWarning
@@ -150,6 +159,91 @@
                  }];
 }
 
+#pragma mark - Keyboard management stuff
+
+- (void) registerForKeyboardNotifications
+{
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void) deregisterForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+- (void) keyboardWillShow:(NSNotification *) notify
+{
+    NSDictionary* info = [notify userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGRect addrFrame = addressTextview.frame;
+    
+    float kbTop = screenRect.size.height - kbSize.height;
+    float tvTop = addrFrame.origin.y;
+    float tvBottom = tvTop + addrFrame.size.height;
+    
+    // check if the keyboard is obsucring anything.
+    if(tvTop > kbTop || tvBottom > kbTop)
+    {
+        // it is, so we need to find out by how much so we can adjust accordingly.
+        float overlap = (tvBottom - kbTop) + 10.0f; // add 10 px so we get a bit of a buffer
+        
+        [UIView animateWithDuration:0.25f
+                              delay:0.0f
+                            options:0
+                         animations:^{
+                             
+                             CGRect curFrame = [[self view] frame];
+                             CGRect adjustedFrame = CGRectMake(curFrame.origin.x,
+                                                               curFrame.origin.y - overlap,
+                                                               curFrame.size.width,
+                                                               curFrame.size.height);
+                             
+                             [[self view] setFrame:adjustedFrame];
+                             
+                         }
+                         completion:nil];
+    }
+    
+    
+}
+
+- (void) keyboardWillBeHidden:(NSNotification *) notify
+{
+    // move all views back to the default position
+    
+    [UIView animateWithDuration:0.25f
+                          delay:0.0f
+                        options:0
+                     animations:^{
+                         
+                         CGRect curFrame = [[self view] frame];
+                         CGRect originalFrame = CGRectMake(0.0f, 0.0f, curFrame.size.width, curFrame.size.height);
+                         
+                         [[self view] setFrame: originalFrame];
+                         
+                     }
+                     completion:nil];
+    
+}
+
 #pragma mark - IBActions
 
 - (IBAction)nameValueChanged:(id)sender
@@ -169,6 +263,5 @@
 {
     [self updateAddressEditButtons:NO];
 }
-
 
 @end
