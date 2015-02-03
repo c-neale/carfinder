3e456cbd466da6906185b5aa134e69843cb26c17
Cory Neale <cory.neale@gmail.com>
2015-02-03 19:36:54 +1100

    base project setup
    Added base project for version 2.0 of app.
    
    ready for a rewrite…

---
+++ wayback/wayback/wayback/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wayback
+//
+//  Created by Cory Neale on 3/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
