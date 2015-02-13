609e8085ef18c5e72561e5d9dec9bc0fb8ab43e3
Cory Neale <cory.neale@gmail.com>
2015-02-13 21:05:52 +1100

    added location provider class

---
+++ wayback/wayback/wayback/LocationProvider.h
@@ -0,0 +1,13 @@
+//
+//  LocationProvider.h
+//  wayback
+//
+//  Created by Cory Neale on 13/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationProvider : NSObject
+
+@end
---
+++ wayback/wayback/wayback/LocationProvider.m
@@ -0,0 +1,13 @@
+//
+//  LocationProvider.m
+//  wayback
+//
+//  Created by Cory Neale on 13/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import "LocationProvider.h"
+
+@implementation LocationProvider
+
+@end
