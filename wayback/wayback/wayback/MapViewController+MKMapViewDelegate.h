7ff170f23accc9ac8d7fa5e79cb4d6ed458e4e67
Cory Neale <cory.neale@gmail.com>
2015-02-13 20:53:56 +1100

    Added class extension for map view delegate

---
+++ wayback/wayback/wayback/MapViewController+MKMapViewDelegate.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController+MKMapViewDelegate.h
+//  wayback
+//
+//  Created by Cory Neale on 13/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <MapKit/MapKit.h>
+#import "MapViewController.h"
+
+@interface MapViewController (MKMapViewDelegate) <MKMapViewDelegate>
+
+@end
---
+++ wayback/wayback/wayback/MapViewController+MKMapViewDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MapViewController+MKMapViewDelegate.m
+//  wayback
+//
+//  Created by Cory Neale on 13/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import "MapViewController+MKMapViewDelegate.h"
+
+@implementation MapViewController (MKMapViewDelegate)
+
+@end
