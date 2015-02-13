315d5b0710051a9d8e3aee79a3cc4bfad90c1a1d
Cory Neale <cory.neale@gmail.com>
2015-02-13 20:37:37 +1100

    Add MapViewController

---
+++ wayback/wayback/wayback/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  wayback
+//
+//  Created by Cory Neale on 13/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ wayback/wayback/wayback/MapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewController.m
+//  wayback
+//
+//  Created by Cory Neale on 13/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
