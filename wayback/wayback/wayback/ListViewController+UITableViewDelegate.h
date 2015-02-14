61b78ffda244fad48ff75d11c3b29246a4cff028
Cory Neale <cory.neale@gmail.com>
2015-02-14 17:29:08 +1100

    Added File for table view delegate

---
+++ wayback/wayback/wayback/ListViewController+UITableViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController+UITableViewDelegate.h
+//  wayback
+//
+//  Created by Cory Neale on 14/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ListViewController_UITableViewDelegate : NSObject
+
+@end
---
+++ wayback/wayback/wayback/ListViewController+UITableViewDelegate.m
@@ -0,0 +1,13 @@
+//
+//  ListViewController+UITableViewDelegate.m
+//  wayback
+//
+//  Created by Cory Neale on 14/02/2015.
+//  Copyright (c) 2015 Cory Neale. All rights reserved.
+//
+
+#import "ListViewController+UITableViewDelegate.h"
+
+@implementation ListViewController_UITableViewDelegate
+
+@end
